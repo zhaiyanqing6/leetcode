45df2cbf82448fbebc88c2a842892ee99e5d0aab
zhaiYanQing <407242373@qq.com>
2017-01-14 18:27:26 +0800

    seach2DMatrixII

---
+++ medium/Search2DMatrixII.java
@@ -0,0 +1,13 @@
+package medium;
+
+public class Search2DMatrixII {
+	public boolean searchMatrix(int[][] matrix, int target) {
+		return false;
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
